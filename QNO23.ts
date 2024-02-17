c4910b01bff474082efad705d58a0439d00efd1a
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 09:39:40 +0500

    Add files via upload

---
+++ QNO23.ts
@@ -0,0 +1,65 @@
+// QNO:23
+//  Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+// let car1: string = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True  otherwise i predict false.");
+// console.log(car1 == 'subaru');
+// console.log(car1 == 'civics');
+
+// let car2: string = 'toyota';
+
+// // console.log("Is car !== 'toyota'? I predict True otherwise i predict false.");
+// // console.log(car2 !== 'toyota');
+// // console.log(car2 !== 'cultus');
+
+// // let com:string="HP computer"
+
+// // console.log("Is com === 'HP computer'? I predict False otherwise i predict true.");
+// // console.log(com === 'HP computer');
+// // console.log(com === 'lenovo computer');
+
+// let book:string="English"
+// console.log("Is book !== 'english'? I predict false otherwise i predict true.");
+// console.log(book !== 'english');
+// console.log(book == "math");
+
+// // let carlenght:number=30
+// // console.log("Is car.length > 4? I predict True.");
+// // console.log(carlenght > 4);
+// // console.log(carlenght > 40);
+
+
+// // console.log("Is car.includes('su')? I predict True.");
+// // console.log(car.includes('su'));
+
+// // console.log("Is car.startsWith('sub')? I predict True.");
+// // console.log(car.startsWith('sub'));
+
+// // console.log("Is car.endsWith('ru')? I predict True.");
+// // console.log(car.endsWith('ru'));
+
+// // console.log("Is car.toLowerCase() == 'subaru'? I predict True.");
+// // console.log(car.toLowerCase() == 'subaru');
+
+// // console.log("Is car.charAt(2) == 'b'? I predict True.");
+// // console.log(car.charAt(2) == 'b');
+
+
+
+
+
+
+
+
+
